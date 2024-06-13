0fa977f9d8100faaaae7c0123a26bccef67c65e7
jilikuo <54220633+jilikuo@users.noreply.github.com>
2024-06-13 11:26:13 -0300

    criação de sistema para limite de movimento do jogador, aperfeiçoamento do código de movimentação

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,109 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //variáveis limitadoras de movimento
     public float stepSize;
+    public float mvCooldown; 
+
+    //variáveis para cáculos internos
     private Vector2 targetPosition;
-    private int direction;
+    private float currentCooldown;
+
+    //variáveis controladoras de funções 
+    private string read = "r";
+    private string update = "u";
+    private string set = "s";
 
     // Start is called before the first frame update
     void Start()
     {
-        targetPosition = transform.position;        
+        PlayerPosition(read);
+        MoveCooldown(set);
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerMovement();
-        transform.position = targetPosition;
+        if (CanMove(currentCooldown)) //se o cooldown está zerado, permite movimento
+        {
+            PlayerMove();
+        }
+        PlayerPosition(read); //sempre recalcula onde o jogador está
     }
 
-    private void playerMovement()
+    private void PlayerMove()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+
+        /* a ideia de colocar um return em cada if é evitar o movimento
+         * completamente diagonal do jogador. */
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             targetPosition += Vector2.up * stepSize;
+            PlayerPosition(update);
+            MoveCooldown(set);
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             targetPosition += Vector2.down * stepSize;
+            PlayerPosition(update);
+            MoveCooldown(set);
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
+        { 
             targetPosition += Vector2.left * stepSize;
+            PlayerPosition(update);
+            MoveCooldown(set);
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             targetPosition += Vector2.right * stepSize;
+            PlayerPosition(update);
+            MoveCooldown(set);
+            return;
+        }
     }
 
+    private void PlayerPosition(string objective)
+    {
+        if (objective == read)
+        {
+            targetPosition = transform.position;
+            return;
+        }
+        if (objective == update)
+        {
+            transform.position = targetPosition;
+            return;
+        }
+    }
+
+    private void MoveCooldown(string objective)
+    {
+        if (objective == set)
+        {
+            currentCooldown = mvCooldown;
+        }
+        if (objective == update)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+    }
+
+    private bool CanMove(float cd)
+    {
+        if (cd > 0)
+        {
+            MoveCooldown(update);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
 }
