277d25c120fc6b07ac2303237eb1828d654a9bab
jilikuo <54220633+jilikuo@users.noreply.github.com>
2024-06-20 22:39:43 -0300

    setting up initial enemy spawn randomness and configurations

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    //variáveis de configuração
     public GameObject easyEnemy;
+    public float spawnCooldown;
+    //variáveis de operação
+    private float cd;
+    private float ySpawn;
+    private float xSpawn;
+    private bool evenCheck;
+    
 
     void Start()
     {
+        cd = spawnCooldown;
         SpawnEnemy(easyEnemy);
     }
 
     void FixedUpdate()
     {
-        
+        TrySpawn();
     }
 
     private void SpawnEnemy(GameObject enemy)
     {
-        Instantiate(enemy, new Vector3(6, 0, 0), Quaternion.identity);
+        ySpawn = Random.Range(0, 8);
+
+        evenCheck = (ySpawn % 2 == 0);
+
+        if (evenCheck) xSpawn =  12;
+        else           xSpawn = -12;
+
+        ySpawn -= 3.5f;
+
+        Instantiate(enemy, new Vector3(xSpawn, ySpawn, 0), Quaternion.identity);
+        cd = spawnCooldown;
     }
 
+    private void TrySpawn()
+    {
+        if (cd <= 0) SpawnEnemy(easyEnemy);
+        else cd -= Time.deltaTime;
+    }
 }
